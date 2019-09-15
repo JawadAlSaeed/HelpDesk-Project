9f4266ce725c503e4467605beed24c85bc3615c6
Jawad Al-Saeed <39744682+JawadAlSaeed@users.noreply.github.com>
2019-09-15 11:14:51 +0300

    fixed the signup.php

--- login/signup.php
+++ login/signup.php
@@ -1,34 +1,19 @@
-<!DOCTYPE html>
-<html>
+<<?php 
+require "header.php";
+ ?>
 
-<head>
-    <title>HelpDesk</title>
-    <link rel="stylesheet" type="text/css" href="../css/mainCSS.css">
-</head>
 
-<body>
-    <header>
-        <a href="#">
-            <img src="../picture/help.png" alt="logo">
-        </a>
-        <ul>
-            <li><a href="home"></a></li>
-            <li><a href="protfolio"></a></li>
-            <li><a href="about me"></a></li>
-            <li><a href="contact"></a></li>
-        </ul>
-        <div>
-            <form action="includes/login.inc.php" method="post">
-                <input type="text" name="mailuid" placeholder="Username/E-mail...">
-                <input type="Password" name="pwd" placeholder="Password">
-                <button type="submit" name="login-submit">Login</button>
-            </form>
-            <a href="signup.php">signup</a>
-            <form action="includes/logout.inc.php" method="post">
-                <button type="submit" name="logout-submit">Login</button>
-            </form>
-        </div>
-    </header>
-</body>
+    <main>
+    <h1>Signup</h1>
+    <form action="includes/signup.inc.php" method="post">
+        <input type="text" name="uid" placeholder="Username">
+        <input type="text" name="mail" placeholder="E-mail">
+        <input type="password" name="pwd" placeholder="password">
+        <input type="password" name="pwd-repeat" placeholder="repeat password">
+        <button type="submit" name="signup-submit">Signup</button>
+    </form>
+    </main>
 
-</html>
+ <?php 
+require "footer.php";
+ ?>
