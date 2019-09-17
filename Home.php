b0832dd9df31725c0fef1afee4477d6e86540f1b
Jawad Al-Saeed <39744682+JawadAlSaeed@users.noreply.github.com>
2019-09-17 14:58:24 +0300

    cleaned up the login

--- Home.php
+++ Home.php
@@ -46,33 +46,35 @@
                     <div>
                         <?php
                         if (isset($_SESSION['userId'])) {
+                            echo '<div class="aligner">';
+                            
                             echo '<form action="includes/logout.inc.php" method="post">
-                            <div style="float: left">
-                                <button id="log" type="submit" name="logout-submit">logout</button>     
+                            <div style="float: right; padding-bottom: 10px;">
+                                <button id="log" type="submit" name="logout-submit">logout</button>
                             </div>
                             </form>';
-                            echo "<article> Welcome back, {$_SESSION['userUid']} &nbsp;&nbsp;</article>";
-                            ;
+                            echo "<article> Welcome, {$_SESSION['userUid']} &nbsp;&nbsp;</article>";
+                            echo '</div>';
                         }
                         else{
                             echo '
                                 <div style="float: left">
                                     <form action="includes/login.inc.php" method="post">
-                                    <div class="group">  
+                                    <div class="group">
                                     <input type="text" name="mailuid" placeholder="Username"> 
                                     <span class="highlight"></span>
-                                    <span class="bar"></span> 
+                                    <span class="bar"></span>
                                     <input type="Password" name="pwd" placeholder="Password">
                                     <span class="highlight"></span>
                                     <span class="bar"></span>
                                     </div>
                                     <button id="log" type="submit" name="login-submit">Login</button>
-                                    
                                     </form>
                                 </div>
                                 <div style="float: right">
                                     <a style="float: right" href="signup.php">Signup</a>
-                                </div>';
+                                </div>
+                            ';
                         }
                         ?>
                     </div>
