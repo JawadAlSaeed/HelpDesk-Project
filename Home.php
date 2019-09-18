25a7e230856a58b9bd36c5eea70d47383383a936
Jawad Al-Saeed <39744682+JawadAlSaeed@users.noreply.github.com>
2019-09-18 14:39:00 +0300

    changed the color scheme

--- Home.php
+++ Home.php
@@ -12,7 +12,6 @@
     <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
     <link href="css/input.css" rel="stylesheet" type="text/css" />
     <link href="css/infoMessages.css" rel="stylesheet" type="text/css" />
-    <link href="css/infoMessages.css" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
