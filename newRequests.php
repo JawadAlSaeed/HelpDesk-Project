adc80e12d32bdd338e42e379674340e76c15b28a
Jawad Al-Saeed <39744682+JawadAlSaeed@users.noreply.github.com>
2019-09-30 10:41:03 +0300

    fixed the popup stuff

--- newRequests.php
+++ newRequests.php
@@ -15,33 +15,6 @@
     <link href="css/infoMessages.css" rel="stylesheet" type="text/css" />
     <link href="css/form.css" rel="stylesheet" type="text/css" />
     <link href="css/popup.css" rel="stylesheet" type="text/css" />
-    <script>
-        // Get the modal
-        var modal = document.getElementById("myModal");
-
-        // Get the button that opens the modal
-        var btn = document.getElementById("myBtn");
-
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close")[0];
-
-        // When the user clicks the button, open the modal 
-        btn.onclick = function() {
-          modal.style.display = "block";
-        }
-
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function() {
-          modal.style.display = "none";
-        }
-
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-          if (event.target == modal) {
-            modal.style.display = "none";
-          }
-        }
-    </script>
 </head>
 
 <body>
@@ -153,13 +126,19 @@
                 </div>
             </form>
             <!-- Trigger/Open The Modal -->
-            <button id="myBtn">Available Departments</button>
+            <button class="popupbutton" id="myBtn">Available Departments</button>
             <!-- The Modal -->
             <div id="myModal" class="modal">
               <!-- Modal content -->
               <div class="modal-content">
                 <span class="close">&times;</span>
-                <p>Some text in the Modal..</p>
+                <b>Available Departments:</b> <br>
+                  <p>Network Department </p>
+                  <p>Support Department </p>
+                  <p>System Department </p>
+                  <small style="float: right">*copy and paste the exact department name from above list</small>
+                  <br>
+
               </div>
             </div>
         </div>
@@ -171,7 +150,33 @@
         <br>
         </center>
     </div>
- 
+        <script>
+        // Get the modal
+        var modal = document.getElementById("myModal");
+
+        // Get the button that opens the modal
+        var btn = document.getElementById("myBtn");
+
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+
+        // When the user clicks the button, open the modal 
+        btn.onclick = function() {
+          modal.style.display = "block";
+        }
+
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+          modal.style.display = "none";
+        }
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+          if (event.target == modal) {
+            modal.style.display = "none";
+          }
+        }
+    </script>
 </body>
 
 </html>
