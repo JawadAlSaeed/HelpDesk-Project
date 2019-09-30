0ff3b6ac95e0827ae915898dd533a23e822e6902
Jawad Al-Saeed <39744682+JawadAlSaeed@users.noreply.github.com>
2019-09-30 09:22:58 +0300

    added popup.css | added the avilalble department button and its code

--- newRequests.php
+++ newRequests.php
@@ -14,6 +14,34 @@
     <link href="css/input.css" rel="stylesheet" type="text/css" />
     <link href="css/infoMessages.css" rel="stylesheet" type="text/css" />
     <link href="css/form.css" rel="stylesheet" type="text/css" />
+    <link href="css/popup.css" rel="stylesheet" type="text/css" />
+    <script>
+        // Get the modal
+        var modal = document.getElementById("myModal");
+
+        // Get the button that opens the modal
+        var btn = document.getElementById("myBtn");
+
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+
+        // When the user clicks the button, open the modal 
+        btn.onclick = function() {
+          modal.style.display = "block";
+        }
+
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+          modal.style.display = "none";
+        }
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+          if (event.target == modal) {
+            modal.style.display = "none";
+          }
+        }
+    </script>
 </head>
 
 <body>
@@ -124,6 +152,16 @@
                     <input type="submit" value="Submit">
                 </div>
             </form>
+            <!-- Trigger/Open The Modal -->
+            <button id="myBtn">Available Departments</button>
+            <!-- The Modal -->
+            <div id="myModal" class="modal">
+              <!-- Modal content -->
+              <div class="modal-content">
+                <span class="close">&times;</span>
+                <p>Some text in the Modal..</p>
+              </div>
+            </div>
         </div>
 
         <br>
