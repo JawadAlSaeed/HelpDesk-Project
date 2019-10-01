210040f24af19712705129ff3b5e69ce2c383ebf
Jawad Al-Saeed <39744682+JawadAlSaeed@users.noreply.github.com>
2019-10-01 11:21:27 +0300

    Update postRequest.php

--- postRequest.php
+++ postRequest.php
@@ -29,7 +29,7 @@
         </div>
         <div class="header">
             <ul>
-                <li><a href="http://localhost/HelpDeskProject/home.php">Home</a></li>
+                <li><a href="Home.php">Home</a></li>
                 <li><a href="Requests.php">Requests</a></li>
                 <li><a href="contactUs.php">Contact us</a></li>
                 <li><a href="FAQ.php">FAQ</a></li>
@@ -75,46 +75,46 @@
         <br><br>
         <div class="subTitle">
             <center>
-                <h1 style="font-size:45px">Request has beensubmited</h1>
+                <h1 style="font-size:45px">Your request has been submited</h1>
                 <br><br>
         </div>
-        
-        <?php
-			$name =  $_POST["name"];
-			$department =  $_POST["department"];
-			$priority =  $_POST["priority"];
-			$description =  $_POST["description"];
+        <div class="postingReqeust">
+            <?php
+    		$name =  $_POST["name"];
+    		$department =  $_POST["department"];
+    		$priority =  $_POST["priority"];
+    		$description =  $_POST["description"];
 
+    		echo "<p><strong>name</strong>: $name<br>";
+    		echo "<strong>department</strong>: $department<br>";
+    		echo "<strong>priority</strong>: $priority <br>";
+    		echo "<strong>description</strong>: $description<br>";
 
-			echo "<p><strong>name</strong>: $name<br />";
-			echo "<strong>department</strong>: $department<br />";
-			echo "<strong>priority</strong>: $priority <br />";
-			echo "<strong>description</strong>: $description<br />";
+    		$DBConnect = mysqli_connect("localhost","root","");
+    		if (!$DBConnect) 
+    		{
+    		    die('Could not connect: ' . mysqli_error());
+    		}
+    		$DBName = "requests";
+    		mysqli_select_db($DBConnect,$DBName);
+    		$QueryString = "INSERT INTO requeststables (name, dpartment, priority, description) VALUES ( '$name','$department','$priority','$description') ";
+    		$QueryResult = mysqli_query($DBConnect,$QueryString)
+    		     Or die("<p> Unable to execute query. </p>"
+    		     . "<p> Error code  " .  mysqli_errno($DBConnect)
+    		     .  ": " . mysqli_error($DBConnect)) . "</p>" ;
+    		echo "<p>Data Successfully uploaded into the table. </p>";
 
-		$DBConnect = mysqli_connect("localhost","root","");
-		if (!$DBConnect) 
-		{
-		    die('Could not connect: ' . mysqli_error());
-		}
-		$DBName = "requests";
-		mysqli_select_db($DBConnect,$DBName);
-		$QueryString = "INSERT INTO requeststables (name, dpartment, priority, description) VALUES ( '$name','$department','$priority','$description') ";
-		$QueryResult = mysqli_query($DBConnect,$QueryString)
-		     Or die("<p> Unable to execute query. </p>"
-		     . "<p> Error code  " .  mysqli_errno($DBConnect)
-		     .  ": " . mysqli_error($DBConnect)) . "</p>" ;
-		echo "<p>Data Successfully uploaded into the table. </p>";
-
-		mysqli_close($DBConnect);
-		 //-------------------------------------------------------------------------
-		?>
-        </form>
-        <hr />
-        <p><a href="home.html">return home</a>
-            <a href="viewRequest.php">View Requests</a></p>
+    		mysqli_close($DBConnect);
+    		 //-------------------------------------------------------------------------
+    		?>
+        </div>
+        <br>
+        <a href="home.html">return home</a>
+        <a href="viewRequest.php">View Requests</a>
 </body>
 
 </html>
+
 <?php 
-require "../footer.php";
- ?>
+require "footer.php";
+?>
