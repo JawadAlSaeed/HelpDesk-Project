6b597965d0f900e0bbf06c639d6bb998d48573f5
Jawad Al-Saeed <39744682+JawadAlSaeed@users.noreply.github.com>
2019-10-01 14:41:46 +0300

    added an title icon to all files

--- postRequest.php
+++ postRequest.php
@@ -7,6 +7,7 @@
     <title>HelpDesk</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/x-icon" href="picture/help.ico" />
     <link href="css/mainCSS.css" rel="stylesheet" type="text/css" />
     <script src="https://kit.fontawesome.com/25823c862e.js"></script>
     <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
