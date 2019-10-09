3ae682a5bb09733f56ed64fd0de27ba8be610c66
Jawad Al-Saeed <39744682+JawadAlSaeed@users.noreply.github.com>
2019-10-09 11:50:50 +0300

    added content to about.php | fixed some code in main.css

--- About.php
+++ About.php
@@ -45,11 +45,46 @@
             <br><br>
         </div>
         <div class="container">
-            
+            Hi, my name is Jawad Al-Saeed, I'm 23 years old, I live in Qatif city in the eastern province. Currently, I'm a trainee at the Al Tuwairqi Holding Company. 
+            <br><br>
+            I'm a student at the Jubail University College (JUC) and major in Computer Science. 
+            <br><br>
+            This is a website/application is for the graduation project. it's intended to show the basics of a help desk website of a company.
+            <br><br>
+            with this website, I'm hoping to show you the programming abilities I'm capable of. 
+            <br><br>
+            the HelpDesk system has two main parts, the website you currently in, and the IT application. First off, well talk about the website. the website contains three main features:
+            <ol>
+                <li>Requests/View Requests</li>
+                <li>Contact Us Form</li>
+                <li>Frequently Asked Questions (FAQs)</li>
+            </ol>
+            <h2>The Website</h2>
+            <h3>Requests/View Requests:</h3>
+            This is a feature that a user can use to request something from the company. In this page a user can do two things:
+            <br><br>
+            <b>New request:</b> Makes a new request with your information.
+            <br><br>
+            <b>View requests:</b> View all old/new requests and their info.
+            <h3>Contact Us Form:</h3>
+            This feature is used to contact the company to suggest something or to give feedback, this suggestion/feedback doesn't require to be implemented.
+            <br>
+            The feedback will be sent to the companies via Email.
+            <h3>FAQs:</h3>
+            Frequently asked questions on the site, these questions are categorized to help users find related questions.
+            <br><br>
+            FAQs have a search feature to find a specific question faster
+            <br><br>
+            these questions can be configured by IT Employees.
+            <h2>The IT Application</h2>
         </div>
         </center>
+
     </div>
- 
+    <br>
+    <br>
+    <br>
+    <br>
 </body>
 
 </html>
