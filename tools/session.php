5701e3ed5a49a16f19f0e68005d4329217f3b6fe
Jawad Al-Saeed <39744682+JawadAlSaeed@users.noreply.github.com>
2019-10-09 12:47:01 +0300

    Update session.php

--- tools/session.php
+++ tools/session.php
@@ -1,10 +1,10 @@
 <?php
     if (isset($_SESSION['userId'])) {
         echo '<li style="float:right">
-                    <form action="includes/logout.inc.php" method="post">
-                        <button id="log" type="submit" name="logout-submit">logout</button>
-                    </form>
-                <li>';
+                <form action="includes/logout.inc.php" method="post">
+                    <button id="log" type="submit" name="logout-submit">logout</button>
+                </form>
+            <li>';
         echo '<li style="float:right"><a href="account.php">Account</a></li>';
         echo "<article> Welcome, {$_SESSION['userUid']} &nbsp;&nbsp;</article>";
     }
